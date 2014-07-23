f0e82458a6cbff48e33ee6af7b8769d98e7ae6f9
Francesca Colombo <Francescarcolombo@gmail.com>
2014-07-23 14:43:53 -0400

    Added links

--- scraper/saks.py
+++ scraper/saks.py
@@ -66,7 +66,46 @@ class SaksCrawler:
 
 	def __init__(self): 
 		print "Creating Saks Crawler"
-		self.crawler = crawler.Crawler(['http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5'], self.getParser())
+		self.crawler = crawler.Crawler(
+			#dresses
+			['http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=180',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=360',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=540',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=720',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=900',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=1080',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=1260',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=1440',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=1620',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=1800',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=1980',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=2160',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=2340',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=2520',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=2700',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=2880',
+			'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=3060']
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=3240',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=3420',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Dresses/shop/_/N-52flor/Ne-6lvnb5?Nao=3600',
+			#tops 
+			
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Tops/shop/_/N-52floo/Ne-6lvnb5',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Tops/shop/_/N-52floo/Ne-6lvnb5?Nao=180',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Tops/shop/_/N-52floo/Ne-6lvnb5?Nao=360',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Tops/shop/_/N-52floo/Ne-6lvnb5?Nao=540',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Tops/shop/_/N-52floo/Ne-6lvnb5?Nao=720',
+			#skirts
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Skirts/shop/_/N-52flos/Ne-6lvnb5',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Skirts/shop/_/N-52flos/Ne-6lvnb5?Nao=180',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Skirts/shop/_/N-52flos/Ne-6lvnb5?Nao=360',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Skirts/shop/_/N-52flos/Ne-6lvnb5?Nao=540',
+			#'http://www.saksfifthavenue.com/Women-s-Apparel/Skirts/shop/_/N-52flos/Ne-6lvnb5?Nao=720']
+
+
+
+			, self.getParser())
 
 
 	def crawl(self):
