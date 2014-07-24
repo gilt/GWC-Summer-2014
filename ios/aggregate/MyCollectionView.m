5a39206d0e7b902d27f21d2055edb1150fb8992e
Francesca Colombo <Francescarcolombo@gmail.com>
2014-07-24 14:48:37 -0400

    Modified ios

---
+++ ios/aggregate/MyCollectionView.m
@@ -0,0 +1,31 @@
+//
+//  MyCollectionView.m
+//  aggregate
+//
+//  Created by Kelsey Wong on 7/22/14.
+//  Copyright (c) 2014 Gilt. All rights reserved.
+//
+
+#import "MyCollectionView.h"
+
+@implementation MyCollectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ ios/aggregate/MyCollectionViewController.h
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewController.h
+//  aggregate
+//
+//  Created by Kelsey Wong on 7/22/14.
+//  Copyright (c) 2014 Gilt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MyCollectionViewCell.h"
+
+@interface MyCollectionViewController : UICollectionViewController
+<UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (strong,nonatomic) NSMutableArray *clothesPics;
+@property (strong,nonatomic) NSMutableArray *clothesNames;
+
+
+@end
