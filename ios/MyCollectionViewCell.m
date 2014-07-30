f1b920499bca670f1997217414000d96aac0b192
Francesca Colombo <Francescarcolombo@gmail.com>
2014-07-30 13:00:40 -0400

    New ios

---
+++ ios/MyCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  MyCollectionViewCell.m
+//  aggregate
+//
+//  Created by Kelsey Wong on 7/22/14.
+//  Copyright (c) 2014 Gilt. All rights reserved.
+//
+
+#import "MyCollectionViewCell.h"
+
+@implementation MyCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
